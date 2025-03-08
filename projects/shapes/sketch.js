d26faa8f6bbd1c6bbbced05aaa74d305a3debd65
Lucas Moura <lucaslpmoura@gmail.com>
2025-03-08 20:48:51 -0300

    Added Rectangles to Shapes

--- projects/shapes/sketch.js
+++ projects/shapes/sketch.js
@@ -1,172 +1,239 @@
-let pointA = [0,0]
-let pointB = [0,0]
-let pointC = [0,0]
-let oldPointA = [0,0];
-let oldPointB = [0,0];
-let oldPointC = [0,0]
 let numOfClicks = 0;
-let currentColor = [0,100,0];
+
+let shapePoints = [];
+
+const TRIANGLE = 3;
+const RECTANGLE = 2;
+
+let selectedShape = TRIANGLE;
+
+let currentColor = [0, 100, 0];
+
 
 let width = 1200;
 let height = 900;
-
 let drawWidth = 0.75 * width;
 
+let shapes = [];
 
-
-let triangles = [];
 let autoGenerate = false;
 let toggleDebug = true;
 
-document.getElementById("auto_button").onclick = autoGenerateTriangles;
-document.getElementById("toggle_debug").onclick = toggleDebugFunc;
-document.getElementById("clear_triangles").onclick = clearTriangles;
-document.getElementById("back_button").onclick = () => { location.href  = "../index.html"};
+document.getElementById("shape_select_button").innerHTML = "Triangles";
+document.getElementById("shape_select_button").onclick = changeSelectedShape;
+document.getElementById("auto_button").onclick = autoGenerateShapes;
+document.getElementById("toggle_debug_button").onclick = toggleDebugFunc;
+document.getElementById("clear_shapes_button").onclick = clearShapes;
+document.getElementById("back_button").onclick = () => { location.href = "../index.html" };
 
-let slider;
+let triangleSlider, rectangleSlider;
 let sliderList = [];
-let baseGenerationInterval = 2000;
+let baseTriangleGenerationInterval = 2000;
+let baseRectangleGenerationInterval = 2000;
 
 function setup() {
-  createCanvas(width, height);
-  slider = createSlider(0, 1500);
-  slider.position(drawWidth + 10, 30); // Generation speed adjustment sliders
-  slider.hide();
-  slider.value(750);
+    createCanvas(width, height);
+    triangleSlider = createSlider(0, 1500);
+    rectangleSlider = createSlider(0, 1500);
+
+    triangleSlider.position(drawWidth + 10, 30); // Generation speed adjustment sliders
+    rectangleSlider.position(drawWidth + 10, 70); // Generation speed adjustment sliders
+
+    triangleSlider.hide();
+    rectangleSlider.hide();
+
+    triangleSlider.value(750);
+    rectangleSlider.value(750);
 }
 
-function draw(){
+function draw() {
 
-  background(127);
-  fill(100,100,100);
-  rect(drawWidth, 0, width, height);
-  if(numOfClicks % 3 == 0 && numOfClicks > 0){
-    //clear();
-    //background(127);
-    //fill(200);
-    generateTriangle(pointA[0], pointA[1], pointB[0], pointB[1], pointC[0], pointC[1], currentColor[0], currentColor[1], currentColor[2]);
-    numOfClicks = 0;
-  }
-
- 
-
-  //triangle(oldPointA[0], oldPointA[1], oldPointB[0], oldPointB[1], oldPointC[0], oldPointC[1])
-  for(let i = 0; i < triangles.length; i++){
-    fill(triangles[i][6], triangles[i][7],triangles[i][8]);
-    triangle(triangles[i][0],triangles[i][1],triangles[i][2],triangles[i][3],triangles[i][4],triangles[i][5]);
-  }
-  drawDebugElements();
-  drawLabels();
-
-}
-
-function mouseClicked(){
-  if(autoGenerate) {
-    return;
-  }
-  if(numOfClicks % 3== 0 ){
-    oldPointA = pointA.slice();
-
-    pointA[0] = mouseX > drawWidth ? drawWidth : mouseX;
-    pointA[1] = mouseY > height ? height : mouseY;
-
-
-  }else if(numOfClicks % 3 == 1){
-    oldPointB = pointB.slice();
-
-    pointB[0] = mouseX > drawWidth ? drawWidth : mouseX;
-    pointB[1] = mouseY > height ? height : mouseY;
-  
-
-  }else if((numOfClicks % 3 == 2)){
-    oldPointC = pointC.slice();
-
-    pointC[0] = mouseX > drawWidth ? drawWidth : mouseX;
-    pointC[1] = mouseY > height ? height : mouseY;
-
-   currentColor[0] = random(0,255);
-   currentColor[1] = random(0,255);
-   currentColor[2] = random(0,255); 
-  }
-  numOfClicks++
-}
-
-function generateTriangle(x1, y1, x2, y2, x3, y3, r, g, b){
-  triangles.push([x1, y1, x2, y2, x3, y3, r, g, b]);
-  oldPointA = pointA.slice();
-  oldPointB = pointB.slice();
-  oldPointC = pointC.slice();
-}
-
-async function autoGenerateTriangles(){
-  //clear();
-  drawDebugElements();
-  numOfClicks = -1;
-  autoGenerate = !autoGenerate;
-
-  if(autoGenerate){
-    document.getElementById("auto_button").innerHTML = "Stop Generating Triangles";
-    showSlider(true);
-    
-  }else{
-    document.getElementById("auto_button").innerHTML = "Auto Gerate Triangles";
-    showSlider(false);
-  }
-
-  while(autoGenerate){
-    promise = await new Promise(resolve => setTimeout(resolve, baseGenerationInterval - slider.value()));
-    console.log("Generating triangle...");
-    if(autoGenerate){
-      generateTriangle(random(0, drawWidth), random(0,height),random(0, drawWidth), random(0,height),random(0, drawWidth), random(0,height),random(0,255),random(0,255),random(0,255));
+    background(127);
+    fill(100, 100, 100);
+    rect(drawWidth, 0, width, height);
+    if (numOfClicks % selectedShape == 0 && numOfClicks > 0) {
+        generateShape(shapePoints, [random(0, 255), random(0, 255), random(0, 255)]);
+        numOfClicks = 0;
     }
-  }
-    
+
+
+
+    //triangle(oldPointA[0], oldPointA[1], oldPointB[0], oldPointB[1], oldPointC[0], oldPointC[1])
+    for (let i = 0; i < shapes.length; i++) {
+        fill(shapes[i][1][0], shapes[i][1][1], shapes[i][1][2]);
+        if (shapes[i][0].length == TRIANGLE) {
+            triangle(
+                shapes[i][0][0][0], shapes[i][0][0][1],
+                shapes[i][0][1][0], shapes[i][0][1][1],
+                shapes[i][0][2][0], shapes[i][0][2][1]
+            )
+        }
+        if (shapes[i][0].length == RECTANGLE) {
+            rectMode(CORNERS);
+            rect(
+                shapes[i][0][0][0], shapes[i][0][0][1],
+                shapes[i][0][1][0], shapes[i][0][1][1]
+            )
+        }
+
+    }
+    drawDebugElements();
+    drawLabels();
+
 }
 
-function drawDebugElements(){
-  if(toggleDebug){
-    fill(200);
+function drawDebugElements() {
+    if (toggleDebug) {
+        fill(200);
 
-  let textSpacing = 20;
-  text (`Point A: ${pointA}`, 10, textSpacing)
-  text (`Old Point A: ${oldPointA}`, 10, textSpacing * 2)
-  text (`Point B: ${pointB}`, 10, textSpacing* 3)
-  text (`Old Point B: ${oldPointB}`, 10, textSpacing * 4)
-  text (`Point C: ${pointC}`, 10, textSpacing* 5)
-  text (`Old Point C: ${oldPointC}`, 10, textSpacing * 6)
-  text (`Num Of Clicks: ${numOfClicks}`, 10, textSpacing * 7)
-  text (`Auto-generate: ${autoGenerate}`, 10, textSpacing * 8)
-  text (`Debug mode: ${toggleDebug}`, 10, textSpacing * 9)
-  }
-  
+        let textSpacing = 20;
+        text(`Num Of Clicks: ${numOfClicks}`, 10, textSpacing * 1)
+        text(`Auto-generate: ${autoGenerate}`, 10, textSpacing * 2)
+        text(`Debug mode: ${toggleDebug}`, 10, textSpacing * 3)
+        text(`Selected Shape: ${selectedShape}`, 10, textSpacing * 4)
+
+        text('Points: ', 10, textSpacing * 5);
+        for (let i = 0; i < shapePoints.length; i++) {
+            text(`${shapePoints[i]}`, 10, textSpacing * (6 + i));
+        }
+
+    }
+
 }
 
-function toggleDebugFunc(){
-  toggleDebug = !toggleDebug;
-  numOfClicks = -1;;
+function mouseClicked() {
+    if (autoGenerate) {
+        return;
+    }
+    if ((numOfClicks % selectedShape == 0)) {
+        shapePoints = [];
+    }
+    if (numOfClicks > -1) {
+        point = [0, 0];
+        point[0] = mouseX > drawWidth ? drawWidth : mouseX;
+        point[1] = mouseY > height ? height : mouseY;
+        shapePoints.push(point.slice());
+    }
+
+
+    numOfClicks++
+    console.log(shapes);
 }
 
-function clearTriangles(){
-  triangles = [];
-  numOfClicks = -1;;
+function generateShape(points, color) {
+    console.log("Generating shape");
+    shapes.push([points, color]);
 }
 
-function showSlider(needToShow){
-  if(needToShow && sliderList.length == 0){
-    slider.show();
-    
-  }
-  if(!needToShow){
-    console.log("Deleting Slider...");
-    slider.hide()
-  }
+function changeSelectedShape() {
+    switch (selectedShape) {
+        case TRIANGLE:
+            document.getElementById("shape_select_button").innerHTML = "Rectangles";
+            selectedShape = RECTANGLE;
+            break;
+        case RECTANGLE:
+            document.getElementById("shape_select_button").innerHTML = "Triangles";
+            selectedShape = TRIANGLE;
+            break;
+        default:
+            selectedShape = TRIANGLE;
+    }
+    numOfClicks = -1;
 }
 
-function drawLabels(){
-  if(autoGenerate){
-    fill(255);
-    text("Triangle Generation Speed", drawWidth + 10, 50); // Triangle Speed Slider label
-    let triangleSpeed = 1/(baseGenerationInterval - slider.value()) * 1000;
-    text(`${triangleSpeed.toFixed(2)} Triangles/s`, drawWidth + 180, 25);
-  }
+async function autoGenerateShapes() {
+    //clear();
+    drawDebugElements();
+    numOfClicks = -1;
+    autoGenerate = !autoGenerate;
+
+    if (autoGenerate) {
+        document.getElementById("auto_button").innerHTML = "Stop Generating Shapes";
+        showSliders(true);
+
+    } else {
+        document.getElementById("auto_button").innerHTML = "Auto Generate Shapes";
+        showSliders(false);
+    }
+
+    if (autoGenerate) {
+        autoGenerateTriangles();
+        autoGenerateRectangles();
+    }
+
+}
+
+async function autoGenerateTriangles() {
+    while (autoGenerate) {
+        promise = await new Promise(resolve => setTimeout(resolve, baseTriangleGenerationInterval - triangleSlider.value()));
+        console.log("Generating triangle...");
+        if (autoGenerate) {
+            generateShape(
+                [
+                    [random(0, drawWidth), random(0, height)],  // Points A, B, C
+                    [random(0, drawWidth), random(0, height)],
+                    [random(0, drawWidth), random(0, height)],
+                ],
+                [
+                    random(0, 255), random(0, 255), random(0, 255), // Color Vector
+                ]
+            )
+        }
+    }
+
+}
+
+async function autoGenerateRectangles() {
+    while (autoGenerate) {
+        promise = await new Promise(resolve => setTimeout(resolve, baseRectangleGenerationInterval - rectangleSlider.value()));
+        console.log("Generating triangle...");
+        if (autoGenerate) {
+            generateShape(
+                [
+                    [random(0, drawWidth), random(0, height)],  // Rectangle Corners
+                    [random(0, drawWidth), random(0, height)],
+                ],
+                [
+                    random(0, 255), random(0, 255), random(0, 255), // Color Vector
+                ]
+            )
+        }
+    }
+
+}
+
+function toggleDebugFunc() {
+    toggleDebug = !toggleDebug;
+    numOfClicks = -1;
+}
+
+function clearShapes() {
+    shapes = [];
+    numOfClicks = -1;
+}
+
+
+function showSliders(needToShow) {
+    if (needToShow && sliderList.length == 0) {
+        triangleSlider.show();
+        rectangleSlider.show();
+
+    }
+    if (!needToShow) {
+        console.log("Deleting Slider...");
+        triangleSlider.hide();
+        rectangleSlider.hide();
+    }
+}
+
+function drawLabels() {
+    if (autoGenerate) {
+        fill(255);
+        text("Triangle Generation Speed", drawWidth + 10, 40); // Triangle Speed Slider label
+        text("Rectangle Generation Speed", drawWidth + 10, 75); // Triangle Speed Slider label
+        let triangleSpeed = 1 / (baseTriangleGenerationInterval - triangleSlider.value()) * 1000;
+        let rectangleSpeed = 1 / (baseRectangleGenerationInterval - rectangleSlider.value()) * 1000;
+        text(`${triangleSpeed.toFixed(2)} Triangles/s`, drawWidth + 180, 25);
+        text(`${rectangleSpeed.toFixed(2)} Rectangles/s`, drawWidth + 180, 55);
+    }
 }
